ec0bb7a512065da4c18f2a8b3276e94b9439645c
A Ranjan <68651277+Ashu-A@users.noreply.github.com>
2024-04-17 13:53:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -96,8 +96,7 @@ with input:
 # wrapper
 wrapper = StreamWrapper(commit_url)
 # client
-client = SpeckleClient('https://speckle.xyz') 
-client.authenticate_with_token(os.getenv('SPECKLE_TOKEN'))
+client = wrapper.get_client()
 # trasnport
 transport = wrapper.get_transport()
 commit = client.commit.get(wrapper.stream_id, wrapper.commit_id)
